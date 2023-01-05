5e07b212266061e796131ce14efe1dc97c5d12c1
uwtrigeo <115599976+uwtrigeo@users.noreply.github.com>
2023-01-05 13:44:08 -0800

    update

--- scripts.js
+++ scripts.js
@@ -1,10 +1,75 @@
-var map = L.map('map').fitWorld();
 
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+alert("This page will ask you to share your location. Please click 'Allow' to continue.");
+
+var light = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'mapbox/streets-v11',
+    id:'mapbox/light-v10',
     accessToken: 'pk.eyJ1IjoidHJpZ2VvIiwiYSI6ImNsOXlmZXlheTA0a3kzdmxuemE5MHVsMnQifQ.abEIHxGUIn-Yz1IwcRTT7Q',
-}).addTo(map);
+    tileSize: 512,
+    zoomOffset: -1,
+});
+
+var dark = L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id:'mapbox/dark-v10',
+    accessToken: 'pk.eyJ1IjoidHJpZ2VvIiwiYSI6ImNsOXlmZXlheTA0a3kzdmxuemE5MHVsMnQifQ.abEIHxGUIn-Yz1IwcRTT7Q',
+    tileSize: 512,
+    zoomOffset: -1,
+});
+
+var map = L.map('map', {layers:[light]}).fitWorld();
+
+function onLocationFound(e) {
+    var radius = e.accuracy; //this defines a variable radius as the accuracy value returned by the locate method. The unit is meters.
+
+    L.marker(e.latlng).addTo(map)  //this adds a marker at the lat and long returned by the locate function.
+        .bindPopup("You are within " + Math.round(radius * 3.28084) + " feet of this point").openPopup(); //this binds a popup to the marker. The text of the popup is defined here as well. Note that we multiply the radius by 3.28084 to convert the radius from meters to feet and that we use Math.round to round the conversion to the nearest whole number.
+
+        if (radius <= 100) {
+            L.circle(e.latlng, radius, {color: 'green'}).addTo(map);
+        }
+        else{
+            L.circle(e.latlng, radius, {color: 'red'}).addTo(map);
+        }
+    var times = SunCalc.getTimes(new Date(), e.latitude, e.longitude);
+    // console.log(times);
+    var sunrise = times.sunrise.getHours();
+    // console.log(sunrise);
+    var sunset = times.sunset.getHours();
+    // console.log(sunset);
+
+    var currentTime = new Date().getHours();
+        if (sunrise < currentTime && currentTime < sunset){
+        map.removeLayer(dark);
+        map.addLayer(light);
+        }
+        else {
+        map.removeLayer(light);
+        map.addLayer(dark);
+        }
+    // console.log(currentTime);
+}
+
+map.on('locationfound', onLocationFound); //this is the event listener
+
+function onLocationError(e) {
+    alert(e.message);
+  }
+  
+map.on('locationerror', onLocationError);
 
 map.locate({setView: true, maxZoom: 16});
+
+var layercontrol = L.control.layers({
+    "Light Map": light,
+    "Dark Map": dark,
+    }, 
+      
+).addTo(map);
+
+// Easy Button
+// L.easyButton('fa-crosshairs fa-lg', function(btn, map){
+//     alert('Map center is at: ' + map.getCenter().toString());
+// }).addTo(map);
